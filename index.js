48f437db2f983501b180f45f3976e9ceec433b8b
Phuong Thanh <phuong74200@gmail.com>
2022-11-19 10:04:17 +0700

    fix cloudflare

--- index.js
+++ index.js
@@ -8,13 +8,12 @@ app.set('view engine', 'pug');
 
 const PORT = process.env.PORT || 7420;
 
-const CloudflareBypasser = require('cloudflare-bypasser');
-const cf = new CloudflareBypasser();
+const cloudscraper = require('cloudscraper');
 
 const getPaging = (id) => {
     const URL = `https://quizlet.com/webapi/3.4/studiable-item-documents?filters%5BstudiableContainerId%5D=${id}&filters%5BstudiableContainerType%5D=1&perPage=555&page=1`;
-    return cf.request(URL)
-        .then(res => JSON.parse(res.body).responses);
+    return cloudscraper.get(URL)
+        .then(res => JSON.parse(res));
 }
 
 const getFullQuizz = async (id) => {
@@ -22,8 +21,8 @@ const getFullQuizz = async (id) => {
         .then(res => res[0].paging.total)
         .then(total => {
             const URL = `https://quizlet.com/webapi/3.4/studiable-item-documents?filters%5BstudiableContainerId%5D=${id}&filters%5BstudiableContainerType%5D=1&perPage=${total}&page=1`;
-            return cf.request(URL)
-                .then(res => JSON.parse(res.body));
+            return cloudscraper.get(URL)
+                .then(res => JSON.parse(res));
         })
 }
 
@@ -38,26 +37,26 @@ function validURL(str) {
 }
 
 app.get('/:id', async (req, res) => {
-    console.log(req.params)
-
     try {
         let id = decodeURIComponent(req.params.id);
 
-        if(validURL(id)) {
+        if (validURL(id)) {
             id = id.match('\/([0-9]+)\/')[1];
         }
 
-        const results = await getFullQuizz(id);
+        const results = await getPaging(id);
 
         console.log(results)
 
         const items = results.responses[0].models.studiableItem
         return res.render('index', { items })
+        // res.json(await getPaging(id))
     } catch (err) {
-        console.log(err);
+        // console.log(err);
         return res.json({
             code: 500,
-            message: 'id not found'
+            message: 'id not found',
+            err: err.message
         });
     }
 });
